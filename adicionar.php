cc6d98e7c1a345bf3578b2676430d4cd8f744100
Igor Tauhan <igortauhan.ds31@gmail.com>
2021-04-11 15:51:54 -0300

    Tela adicionar feita

---
+++ adicionar.php
@@ -0,0 +1,32 @@
+<?php
+//header
+include_once 'includes/header.php';
+?>
+
+<div class="row">
+	<div class="col 12 m6 push-m3">
+		<h3>Adicionar gasto</h3>
+		<form action="create.php" method="POST">
+			<div class="input-field col s12">
+				<input type="text" name="descricao" id="descricao">
+				<label for="descricao">Descrição</label>
+			</div>
+			<div class="input-field col s12">
+				<input type="text" name="valor" id="valor">
+				<label for="valor">Valor</label>
+			</div>
+			<div class="input-field col s12">
+				<input type="text" name="data" id="data">
+				<label for="data">Data</label>
+			</div>
+			<button type="submit" name="btn-adicionar" class="btn"> Adicionar </button>
+			<a href="index.php" class="btn green">Lista de gastos</a>
+		</form>
+		<br>
+	</div>
+</div>
+
+<?php
+//footer
+include_once 'includes/footer.php';
+?>
